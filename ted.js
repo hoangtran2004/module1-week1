9ae562b228808700eab4001e1f8e6ca75074227a
hoangtran2004 <tranhuyhoang58825@gmail.com>
2022-08-17 22:50:01 +0700

    taofromdongian

---
+++ ted.js
@@ -0,0 +1,6 @@
+inputWidth=prompt("enter the width");
+inputHeight=prompt("enter the height");
+let Width=parseInt(inputWidth);
+let Height=parseInt(inputHeight);
+let area = width * height;
+document.write("the area is:  "+area)
